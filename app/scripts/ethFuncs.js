9d001a59c580008fd2e764f011741e92089e715d
Zwilla <info@zwilla.de>
2017-06-22 00:34:48 +0200

    new Array() to []
    CSP! And it works perfect, cleanup later.

--- app/scripts/ethFuncs.js
+++ app/scripts/ethFuncs.js
@@ -61,7 +61,7 @@ ethFuncs.getDeteministicContractAddress = function(address, nonce) {
 ethFuncs.padLeft = function(n, width, z) {
     z = z || '0';
     n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    return n.length >= width ? n : [width - n.length + 1].join(z) + n;
 }
 ethFuncs.getDataObj = function(to, func, arrVals) {
     var val = "";
