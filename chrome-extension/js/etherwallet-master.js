2ed723df6cd576f4307c88341d297e97b7fd1b16
Cody Burns <cody.w.burns@gmail.com>
2017-07-09 14:58:12 -0500

    clean up randnumber msg

--- chrome-extension/js/etherwallet-master.js
+++ chrome-extension/js/etherwallet-master.js
@@ -871,18 +871,10 @@ var genericCtrl = function ($scope, globalService, $sce) {
     }, 5000);
 
     var randNumber = Math.floor(Math.random() * 5) + 1;
-    console.log(randNumber);
-    if (randNumber == 1) {
-        $scope.loadingMsg = { msg: 'WELCOME_msg1', website: window.location.href };
-    } else if (randNumber == 2) {
-        $scope.loadingMsg = { msg: 'WELCOME_msg2', website: window.location.href };
-    } else if (randNumber == 3) {
-        $scope.loadingMsg = { msg: 'WELCOME_msg3', website: window.location.href };
-    } else if (randNumber == 4) {
-        $scope.loadingMsg = { msg: 'WELCOME_msg4', website: window.location.href };
-    } else {
-        $scope.loadingMsg = { msg: 'WELCOME_msg5', website: window.location.href };
-    }
+    $scope.loadingMsg = {
+        msg: 'WELCOME_msg' + randNumber,
+        website: window.location.href
+    };
 };
 module.exports = genericCtrl;
 
