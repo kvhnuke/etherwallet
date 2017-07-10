26366926c3121e46f65b52c02bace6db1551cd64
Nguyen Tien Si <info@nguyentiensi.com>
2017-07-10 11:35:48 +0700

    Format the php code

--- json_relay_php/TheDAO/getDAOProposals.php
+++ json_relay_php/TheDAO/getDAOProposals.php
@@ -8,51 +8,54 @@ require_once '../libs/jsonRPCClient.php';
 $gethRPC = new jsonRPCClient('http://45.79.107.116:8545');
 $req = $_REQUEST['req'];
 if ($req == "force") {
-    $proposals = array();
+    $proposals = [];
     $pCount = getProposalCount();
     for ($i = 0; $i <= $pCount; $i++) {
-        $proposals[] = array(
+        $proposals[] = [
             "proposalID" => $i,
-            "data" => getProposal($i),
-            );
+            "data"       => getProposal($i),
+        ];
     }
     file_put_contents(CACHE_FILE, json_encode($proposals, JSON_PRETTY_PRINT));
-} 
-else if ($req == "update") {
-    $proposals = json_decode(file_get_contents(CACHE_FILE), 1);
-    $pCount = getProposalCount();
-    if ($pCount != count($proposals) - 1) {
-        for ($i = count($proposals); $i <= $pCount; $i++) {
-            $proposals[] = array(
-                "proposalID" => $i,
-                "data" => getProposal($i),
-            );
-        }
-        file_put_contents(CACHE_FILE, json_encode($proposals, JSON_PRETTY_PRINT));
-    }
 }
 else {
-    header('Content-Type: application/json');
-    echo file_get_contents(CACHE_FILE);
+    if ($req == "update") {
+        $proposals = json_decode(file_get_contents(CACHE_FILE), 1);
+        $pCount = getProposalCount();
+        if ($pCount != count($proposals) - 1) {
+            for ($i = count($proposals); $i <= $pCount; $i++) {
+                $proposals[] = [
+                    "proposalID" => $i,
+                    "data"       => getProposal($i),
+                ];
+            }
+            file_put_contents(CACHE_FILE, json_encode($proposals, JSON_PRETTY_PRINT));
+        }
+    }
+    else {
+        header('Content-Type: application/json');
+        echo file_get_contents(CACHE_FILE);
+    }
 }
-function getProposal($id)
-{
-    $getProposal = array(
+function getProposal($id) {
+    $getProposal = [
         "data" => PROPOSAL_GET . str_pad(bcdechex($id), 64, "0", STR_PAD_LEFT),
-        "to" => DAO_ADDR,
-        );
+        "to"   => DAO_ADDR,
+    ];
+
     return json_decode(getEthCall($getProposal, $GLOBALS['gethRPC']), 1)['data'];
 }
-function getProposalCount()
-{
-    $getCount = array(
+
+function getProposalCount() {
+    $getCount = [
         "data" => PROPOSAL_COUNT,
-        "to" => DAO_ADDR,
-        );
+        "to"   => DAO_ADDR,
+    ];
+
     return bchexdec(json_decode(getEthCall($getCount, $GLOBALS['gethRPC']), 1)['data']);
 }
-function getEthCall($txobj, $gethRPC)
-{
+
+function getEthCall($txobj, $gethRPC) {
     $data = getDefaultResponse();
     try {
         $data['data'] = getRPCResponse($gethRPC->eth_call($txobj, "pending"));
@@ -61,41 +64,47 @@ function getEthCall($txobj, $gethRPC)
         $data['error'] = true;
         $data['msg'] = $e->getMessage();
     }
+
     return json_encode($data);
 }
-function bchexdec($hex)
-{
+
+function bchexdec($hex) {
     $dec = 0;
     $len = strlen($hex);
     for ($i = 1; $i <= $len; $i++) {
         $dec = bcadd($dec, bcmul(strval(hexdec($hex[$i - 1])), bcpow('16', strval($len -
             $i))));
     }
+
     return $dec;
 }
-function bcdechex($dec)
-{
+
+function bcdechex($dec) {
     $hex = '';
     do {
         $last = bcmod($dec, 16);
         $hex = dechex($last) . $hex;
         $dec = bcdiv(bcsub($dec, $last), 16);
     } while ($dec > 0);
+
     return $hex;
 }
-function getDefaultResponse()
-{
+
+function getDefaultResponse() {
     $data['error'] = false;
     $data['msg'] = "";
     $data['data'] = "";
+
     return $data;
 }
-function getRPCResponse($result)
-{
+
+function getRPCResponse($result) {
     if (isset($result['result'])) {
         return $result['result'];
-    } else {
+    }
+    else {
         throw new Exception($result['error']['message']);
     }
 }
+
 ?>
